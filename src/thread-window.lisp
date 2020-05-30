32727cf0ae3f75f2b99388a2d67b301d57f51ff6
cage <cage-invalid@invalid>
2020-05-30 11:53:17 +0200

    - [breaking changes] changed table for attachments; - fixed  bug  that prevented  statuses  missing  attachments when  the   status was reblogged.

--- src/thread-window.lisp
+++ src/thread-window.lisp
@@ -780,14 +780,16 @@ db:renumber-timeline-message-index."
              (refresh-event (make-instance 'program-events:refresh-conversations-window-event)))
         (multiple-value-bind (reblogged-status-body reblogged-status-attachments)
             (reblogged-data fields)
-          (let ((actual-body (if (string= body reblogged-status-body)
-                                 body
-                                 (strcat  body reblogged-status-body))))
+          (let ((actual-body        (if (string= body reblogged-status-body)
+                                        body
+                                        (strcat body reblogged-status-body)))
+                (actual-attachments (if (string= attachments reblogged-status-attachments)
+                                        attachments
+                                        (strcat reblogged-status-attachments attachments))))
             (setf (message-window:source-text *message-window*)
                   (strcat header
                           actual-body
-                          reblogged-status-attachments
-                          attachments))
+                          actual-attachments))
             (db:mark-status-red-p timeline-type timeline-folder status-id)
             (resync-rows-db object :redraw t)
             (program-events:push-event refresh-event)
