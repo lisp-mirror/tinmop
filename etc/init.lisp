ade9ed3473f80c5cc61a535f9cf5d14d7309192d
cage <cage@invalid>
2021-05-23 10:50:58 +0200

    - [gemini] reverted tour mode keys to old ones "t t" -> "t"
      there is usually no needs for  explictly open a TOC gemini window as
      it is opened when the gemini stream is completed.

--- etc/init.lisp
+++ etc/init.lisp
@@ -351,9 +351,9 @@
 
 (define-key "p"         #'message-toggle-preformatted-block          *gemini-message-keymap*)
 
-(define-key "t t"       #'next-tour-link                             *gemini-message-keymap*)
+(define-key "t"         #'next-tour-link                             *gemini-message-keymap*)
 
-(define-key "t o"       #'open-gemini-toc                            *gemini-message-keymap*)
+(define-key "O"         #'open-gemini-toc                            *gemini-message-keymap*)
 
 (define-key "T"         #'show-tour-links                            *gemini-message-keymap*)
 
