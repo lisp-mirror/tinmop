2f59f589f13eb82f705b2c6cb99d5d7f046860f8
cage <cage@invalid>
2021-11-14 16:34:47 +0100

    - fixed typo.

--- scripts/get-following.lisp
+++ scripts/get-following.lisp
@@ -1,4 +1,4 @@
-;; geat a list of people you are following
+;; get a list of people you are following
 ;; Copyright © 2021 cage
 
 ;; This program is free software: you can redistribute it and/or modify
